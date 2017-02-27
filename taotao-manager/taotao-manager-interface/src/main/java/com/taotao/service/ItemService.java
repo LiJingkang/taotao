4bc5aca1fd44786d4775e0402e3efe20b3faf5e9
LJK_HaseeD3 <LiJingkang@outlook.com>
2017-02-27 15:27:26 +0800

    配置Dubbo服务。Service和Web层分别启动成功。

---
+++ taotao-manager/taotao-manager-interface/src/main/java/com/taotao/service/ItemService.java
@@ -0,0 +1,11 @@
+package com.taotao.service;
+
+import com.taotao.pojo.TbItem;
+
+/**
+ * Created by Skying on 2017/2/27.
+ */
+public interface ItemService {
+
+    TbItem getItemById(long itemId);
+}
