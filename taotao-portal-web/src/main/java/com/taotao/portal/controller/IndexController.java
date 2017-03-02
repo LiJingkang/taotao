8f6c463c63fe866a19b385a1e24b15bca1510483
LJK_HaseeD3 <LiJingkang@outlook.com>
2017-03-02 15:35:21 +0800

    同上

--- taotao-portal-web/src/main/java/com/taotao/portal/controller/IndexController.java
+++ taotao-portal-web/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     // 后台不拦截 / 只拦截 .html
+    // 当寻找index.html的时候没有找到，会调用这个方法。
     @RequestMapping("/index")
     public String showIndex() {
         return "index";
