de9d6c1e0d2390252c8c5b4953d553ec5c057d8a
LJK_HaseeD3 <LiJingkang@outlook.com>
2017-03-05 19:42:39 +0800

    修改依赖错误。

---
+++ taotao-search/taotao-search-service/src/test/java/com/taotao/solrj/TestSolrJ.java
@@ -0,0 +1,16 @@
+package com.taotao.solrj;
+
+import com.sun.tracing.dtrace.ArgsAttributes;
+import org.junit.Test;
+
+/**
+ * Created by Skying on 2017/3/5.
+ */
+public class TestSolrJ {
+
+    @Test
+    public void testAddDocument() throws Exception {
+
+    }
+
+}
