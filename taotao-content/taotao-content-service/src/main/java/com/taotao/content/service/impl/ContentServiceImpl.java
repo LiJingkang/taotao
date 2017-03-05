0f66fe9fc8063ccd1201bfa3b3f4149a367c2eae
LJK_HaseeD3 <LiJingkang@outlook.com>
2017-03-05 11:58:49 +0800

    添加使用缓存代码

--- taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
+++ taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
@@ -1,12 +1,16 @@
 package com.taotao.content.service.impl;
 
 import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.JsonUtils;
 import com.taotao.content.service.ContentService;
+import com.taotao.jedis.JedisClient;
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
 import com.taotao.pojo.TbContentCategoryExample;
 import com.taotao.pojo.TbContentExample;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -18,8 +22,13 @@ import java.util.List;
 @Service
 public class ContentServiceImpl implements ContentService {
 
+    @Value("${INDEX_CONTENT}")
+    private String INDEX_CONTENT;
+
     @Autowired
     private TbContentMapper contentMapper;
+    @Autowired
+    private JedisClient jedisClient;
 
 
     @Override
@@ -35,11 +44,34 @@ public class ContentServiceImpl implements ContentService {
 
     @Override
     public List<TbContent> getContentByCid(long cid) {
+        // 先查询缓存
+        // 添加缓存不能影响正常的业务罗技
+        try {
+            // 查询缓存
+            // 查询到结果，把json转换为List返回
+            String json = jedisClient.hget(INDEX_CONTENT,cid + "");
+            if (StringUtils.isNotBlank(json)) {
+                List<TbContent> list = JsonUtils.jsonToList(json, TbContent.class);
+                return list;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 缓存中没有命中 需要查询数据亏
         TbContentExample example = new TbContentExample();
         TbContentExample.Criteria criteria = example.createCriteria();
+        // 设置查询条件
         criteria.andCategoryIdEqualTo(cid);
         // 执行查询
         List<TbContent> list = contentMapper.selectByExample(example);
+        // 把结果添加到缓存
+        try {
+            // 加入缓存
+            jedisClient.hset(INDEX_CONTENT, cid + "", JsonUtils.objectToJson(list));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 返回结果
         return list;
     };
 }
