f44644dad86b65c51346f6085d9dc5e95cb05cf7
LJK-WORK-D3\LJK_Work <LiKangSkying@outlook.com>
2017-09-20 13:21:43 +0800

    bug fix

--- taotao-item-web/src/main/java/com/taotao/item/controller/ItemController.java
+++ taotao-item-web/src/main/java/com/taotao/item/controller/ItemController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * 商品详情页面展示Controller
